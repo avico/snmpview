8fff047cdec4e362bbc9847a0bedc010ce404868
Andrey V Ivanov <andrew@sun-ws.ossrc>
2012-06-22 15:14:58 +0359

    model changed, added simple view to show 50 alarms from db

--- snmpview/index/models.py
+++ snmpview/index/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Alist(models.Model):
-    datetime = models.DateTimeField(null=True, blank=True)
+    datetime = models.DateTimeField(primary_key=True, blank=True)
     target = models.CharField(max_length=60, blank=True)
     problem = models.CharField(max_length=60, blank=True)
     trap = models.CharField(max_length=100, blank=True)
@@ -10,3 +10,4 @@ class Alist(models.Model):
 
     class Meta:
         db_table = u'alist'
+        ordering = ["-datetime"]
--- snmpview/index/views.py
+++ snmpview/index/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from snmpview.index.models import Alist
 
 def index(request):
-    return HttpResponse("Test index page")
+	alarms = Alist.objects.using('alist')[:50]
+        return render_to_response('index.html',{'alarms': alarms})
